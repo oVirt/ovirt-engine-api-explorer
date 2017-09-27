622a4dd150b9b9c3c4345d0cb971feb21724b2c4
Juan Hernandez <juan.hernandez@redhat.com>
2017-09-27 23:12:32 +0200

    Re-add RPM build
    A previous patch removed the files related to building RPM packages
    because the project wasn't prepared yet to do that. Since then the
    project has been prepared, and the 'ovirt-engine-nodejs-modules' package
    has been updated to include the modules required by this project. This
    patch re-adds the RPM building.
    
    Change-Id: I4d164fe8d2b7871658a7b2ffa8d94963672c06ed
    Signed-off-by: Juan Hernandez <juan.hernandez@redhat.com>

--- automation/check-patch.sh
+++ automation/check-patch.sh
@@ -1,10 +1,1 @@
-#!/bin/sh -ex
-
-# Add the node and yarn binary directories to the path:
-PATH="${PATH}:/usr/share/ovirt-engine-nodejs/bin"
-PATH="${PATH}:/usr/share/ovirt-engine-yarn/bin"
-
-# Test and build the application:
-yarn install
-yarn test
-yarn build
+build.sh
