985a15cb4c9a822daf6978614c05d4ff05cc03eb
Juan Hernandez <juan.hernandez@redhat.com>
2017-09-27 16:41:54 +0200

    Fix build issues
    Currently the automated build of the project in Jenkins is failing
    because the project is not yet prepared to build using the oVirt Node.js
    modules. In addition the project will not be distributed using RPM. This
    patch removes the RPM .spec files and changes the build process so that
    it uses plain yarn to build and test, which should work without
    problems.
    
    Later patches will adjust the build so that it uses the oVirt Node.js
    modules.
    
    Change-Id: Ife3611ebe95cf5161771f10b99e2efd9ff609700
    Signed-off-by: Juan Hernandez <juan.hernandez@redhat.com>

--- automation/build-artifacts.sh
+++ automation/build-artifacts.sh
@@ -4,29 +4,26 @@
 rm -rf exported-artifacts
 mkdir -p exported-artifacts
 
+# Add the node and yarn binary directories to the path:
+PATH="${PATH}:/usr/share/ovirt-engine-nodejs/bin"
+PATH="${PATH}:/usr/share/ovirt-engine-yarn/bin"
+
 # Calculate the version number:
 version="$(automation/version.py)"
-date="$(date --utc +%Y%m%d)"
+date="$(date -u +%Y%m%d)"
 commit="$(git log -1 --pretty=format:%h)"
 suffix=".${date}git${commit}"
 
-# Build the tar file:
+# Build the application:
+yarn install
+yarn build
+
+# Build the tarball:
 tar_name="ovirt-engine-api-explorer"
-tar_prefix="${tar_name}-${version}/"
+tar_dir="${tar_name}-${version}"
 tar_file="${tar_name}-${version}${suffix}.tar.gz"
-git archive --prefix="${tar_prefix}" --output="${tar_file}" HEAD
+mv build "${tar_dir}"
+tar czf "${tar_file}" "${tar_dir}"
 
-# Build the RPM:
-mv "${tar_file}" packaging/
-pushd packaging
-  export tar_version="${version}"
-  export tar_file
-  export rpm_suffix="${suffix}"
-  ./build.sh
-popd
-
-# Copy the .tar.gz and .rpm files to the exported artifacts directory:
-for file in $(find . -type f -regex '.*\.\(tar.gz\|rpm\)'); do
-  echo "Archiving file \"$file\"."
-  mv "$file" exported-artifacts/
-done
+# Copy the tarball to the exported artifacts directory:
+mv "${tar_file}" exported-artifacts/
--- automation/check-patch.sh
+++ automation/check-patch.sh
@@ -1,3 +1,10 @@
 #!/bin/sh -ex
 
-# Nothing yet.
+# Add the node and yarn binary directories to the path:
+PATH="${PATH}:/usr/share/ovirt-engine-nodejs/bin"
+PATH="${PATH}:/usr/share/ovirt-engine-yarn/bin"
+
+# Test and build the application:
+yarn install
+yarn test
+yarn build
